91d62876fbb17f6f5695b60bd8454ff96c8d94a4
Atul1305 <atulcsakg@gmail.com>
2023-02-07 16:45:57 +0000

    fixed outofbound exception

--- src/general/NumOfIslands.java
+++ src/general/NumOfIslands.java
@@ -8,8 +8,15 @@
  public class NumOfIslands {
 
     public static void main(String[] args) {
-        int[][] matrix = {{0,1},{1,0},{1,1},{1,0}};
-        System.out.println(numIslands(matrix, 2, 4));
+        //int[][] matrix = {{0,1},{1,0},{1,1},{1,0}};
+
+        int[][] matrix = {{1, 1, 0, 0, 0},
+                           {0, 1, 0, 0, 1},
+                           {1, 0, 0, 1, 1},
+                          {0, 0, 0, 0, 0},
+                         {1, 0, 1, 0, 0}}; // TODO : Expected output is 5, but giving 6
+                         
+        System.out.println(numIslands(matrix, 5, 5));
     }
 
     public static int numIslands(int[][] matrix, int n, int m) {
@@ -20,16 +27,16 @@
                 sum = sum + countIslands(matrix, visited, n, m, i, j);
             }
         }
-        return 0;
+        return sum;
     }
 
     private static int countIslands(int[][] matrix, int[][] visited, int n, int m, int i, int j) {
         int sum = 0;
-        if(i>=n || j >= m) {
+        if(i==n || j == m) {
             return 0;
         }
-        while(matrix[i][j] == 1 && visited[i][j]==0) {
-            sum=1;
+        if (matrix[i][j] == 1 && visited[i][j]==0) {
+            sum = 1;
             visited[i][j] = 1;
             countIslands(matrix, visited, n, m, i, j+1);
             countIslands(matrix, visited, n, m, i+1, j);
