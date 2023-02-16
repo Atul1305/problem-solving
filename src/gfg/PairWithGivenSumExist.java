ed321a08c54749585afaf7ff6f0a551c80c0ff05
Atul1305 <atulcsakg@gmail.com>
2023-02-16 16:15:26 +0000

    Added PairWithGivenSum

---
+++ src/gfg/PairWithGivenSumExist.java
@@ -0,0 +1,24 @@
+package gfg;
+/*
+ * Problem Link : https://www.geeksforgeeks.org/given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x/
+ * @since 16/02/2023
+ * 
+ * Input: arr[] = {0, -1, 2, -3, 1}, x= -2
+ * Output: Yes
+ * Explanation:  If we calculate the sum of the output,1 + (-3) = -2
+ * 
+ */
+
+ public class PairWithGivenSumExist {
+
+    public static void main(String[] args) {
+        process(new int[]{ 1,7,2,10, -8, 67, 100, 45, 82 }, 2);
+    }
+
+    // TODO Complete this method.
+    public static String process(int[] arr, int target) {
+
+        return "No";
+    }
+ }
+ 
