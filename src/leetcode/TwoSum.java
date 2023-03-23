fd326a5fa55ada2661e2d48efc6503135888191f
Atul1305 <atulcsakg@gmail.com>
2023-03-23 15:10:18 +0000

    Two sum problem solution - All test cases not passed

---
+++ src/leetcode/TwoSum.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * 1. Two Sum
+ * Link -> https://leetcode.com/problems/two-sum/description/
+ * @since March 23rd, 2023
+ * 
+ * 
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.twoSum(new int[]{3,2,4}, 6);
+
+        for (int i=0; i < result.length; i++) {
+            System.out.println(result);
+        }
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[]{-1, -1};
+
+        Map<Integer, Integer> indexer = new HashMap<>();
+
+        for (int i=0; i<nums.length; i++) {
+            int find = target - nums[i];
+            if(indexer.containsKey(find)) {
+                result[0] = indexer.get(find);
+                result[1] = 1;
+                break;
+            }
+            indexer.put(nums[i], i);
+            
+        }
+        return result;
+    }
+    
+}
