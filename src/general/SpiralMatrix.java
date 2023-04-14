f1dd7974849e44ddcfc7b9166b6fae511a47ed5d
Atul1305 <atulcsakg@gmail.com>
2023-04-14 16:38:12 +0000

    Initial Commit for Spiral Matrix.

---
+++ src/general/SpiralMatrix.java
@@ -0,0 +1,17 @@
+package general;
+
+/**
+ * Problem Link : https://www.educative.io/answers/spiral-matrix-algorithm
+ * @since 14/04/2023
+ */
+public class SpiralMatrix {
+
+    public static void main(String[] args) {
+        System.out.println(spiralMatrix());
+    }
+
+    private static int[] spiralMatrix() {
+    
+    }
+        
+}
