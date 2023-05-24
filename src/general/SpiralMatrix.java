9b4f6494f5f5a03efa33fd2d6f3cfbc3a1cab10a
Atul1305 <atulcsakg@gmail.com>
2023-05-24 14:14:30 +0000

    SpiralMatrix - Work in progress

--- src/general/SpiralMatrix.java
+++ src/general/SpiralMatrix.java
@@ -7,11 +7,40 @@ package general;
 public class SpiralMatrix {
 
     public static void main(String[] args) {
-        System.out.println(spiralMatrix());
+        int[][] mat = { {1},
+                        {4},
+                        {7}};
+        Set<Integer> spiralMatrixSet = new LinkedHashSet<>();
+        int x1=0, x2=1, y1=0, y2=3;
+
+        // TODO : Think about this condition.
+        while(x1<x2) {
+            addBoundaryElement(mat, x1, x2, y1, y2, spiralMatrixSet);
+            x1++;
+            x2--;
+            y1++;
+            y2--;
+        }
+
+        System.out.println("Spiral Matrix is - " + spiralMatrixSet);
     }
 
-    private static int[] spiralMatrix() {
-    
+    private static void addBoundaryElement(int[][] mat, int x1, int x2, int y1, int y2, Set<Integer> spiralMatrixSet) {
+
+        for (int i = x1; i < x2; i++) {
+            spiralMatrixSet.add(mat[i][y1]);
+        }
+        for (int i = y1; i < y2; i++) {
+            spiralMatrixSet.add(mat[x2-1][i]);
+        }
+        for (int i = x2-1; i > x1; i--) {
+            spiralMatrixSet.add(mat[i][y2-1]);
+        }
+        for (int i = y2-1; i > y1; i--) {
+            spiralMatrixSet.add(mat[x1][i]);
+        }
+
+
     }
         
 }
